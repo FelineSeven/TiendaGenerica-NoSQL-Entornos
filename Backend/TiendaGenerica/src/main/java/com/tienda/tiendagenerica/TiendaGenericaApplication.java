c35848f45c583094e17013d52ca4b6c6c72b0709
FelineSeven <fabiacupez.09@gmail.com>
2025-11-11 23:11:40 -0500

    First Commit

---
+++ Backend/TiendaGenerica/src/main/java/com/tienda/tiendagenerica/TiendaGenericaApplication.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.tiendagenerica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaGenericaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaGenericaApplication.class, args);
+    }
+}
+
